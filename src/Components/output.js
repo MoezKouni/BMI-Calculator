4e2d1377ed3458547889b19a1e230b0980e37de3
MoezKouni <mo.elkouni@gmail.com>
2018-05-10 14:06:38 +0100

    BMI Calculator

---
+++ src/Components/output.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Output extends Component {
+  render() {
+    let height = this.props.data.height;
+    let weight = this.props.data.weight;
+    let bmi = this.props.data.bmi;
+    let bmiClass = this.props.data.bmiClass;
+    return (
+      <div className="output">
+        <h3>{height}</h3>
+        <h3>{weight}</h3>
+        <h3>{bmi}</h3>
+        <h3> {bmiClass}</h3>
+      </div>
+    );
+  }
+}
+
+export default Output;
